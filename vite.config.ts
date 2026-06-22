72b5d0646af0b9aab3407fed7acd66860984bf3a
harissfx <hs6115308@gmail.com>
2026-06-22 21:37:19 +0700

    fix: update vite base path to relative

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    // base: './' memastikan path aset bersifat relatif, 
-    // ini kunci agar website tidak putih saat di-deploy
-    base: './', 
-    
-    plugins: [
-      react(), 
-      tailwindcss()
-    ],
-    
+    base: './',
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@': path.resolve(__dirname, '.'),
       },
     },
-    
-    build: {
-      outDir: 'dist', // Pastikan Vercel membaca folder ini
-    }
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
   };
 });
