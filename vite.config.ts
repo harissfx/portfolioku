9ae0e3e450ddbcef2cdb0b9eb030f4a01b9e3b43
harissfx <hs6115308@gmail.com>
2026-06-23 16:56:28 +0700

    fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // WAJIB: Sesuaikan dengan nama repo untuk GitHub Pages
-  base: '/portfolioku/', 
+  base: '/',
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist', // Pastikan ini adalah folder hasil build
+    outDir: 'dist',
   }
 });
